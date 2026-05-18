a6eb5c0a11517ce12284f0916e5928f65dfb316f
KimNayeon <gameya120@gmail.com>
2026-05-18 18:34:14 +0900

    second commit
    Add 4th push

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("이번엔 진짜로 진짜로 성공한다 제발");
             Console.WriteLine("실시간으로 저장이 되고 있을까요?");
             Console.WriteLine("최신화를 시도합니다.");
+            Console.WriteLine("추가 수정했습니다. 깃허브 추가 푸쉬가 가능할까요?");
         }
     }
 }
