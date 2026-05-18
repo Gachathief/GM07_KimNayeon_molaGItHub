cc7e5102928c500014d11164ac597fb7ceb7a0c7
KimNayeon <gameya120@gmail.com>
2026-05-18 18:12:38 +0900

    first commit
    Add new cw

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace day21_re
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("이번엔 진짜로 진짜로 성공한다 제발");
+            Console.WriteLine("실시간으로 저장이 되고 있을까요?");
+            Console.WriteLine("최신화를 시도합니다.");
+        }
+    }
+}
