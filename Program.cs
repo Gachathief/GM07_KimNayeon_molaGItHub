f68759245f41f266c45eed8f41404976769fba47
KimNayeon <gameya120@gmail.com>
2026-05-18 18:37:48 +0900

    third commit
    Add last

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine("실시간으로 저장이 되고 있을까요?");
             Console.WriteLine("최신화를 시도합니다.");
             Console.WriteLine("추가 수정했습니다. 깃허브 추가 푸쉬가 가능할까요?");
+            Console.WriteLine("추가 수정 성공, 마지막 실험.");
         }
     }
 }
